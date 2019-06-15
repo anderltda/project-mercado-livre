65128b3b656c3e8bc7a57b4285c4385ec09f7f0e
anderson <anderltda@gmail.com>
2019-06-14 21:27:38 -0300

    Updates

---
+++ precache-manifest.fce1d652e69a7cf821326aa822f4f338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6ffb9087c4cc2d7815f275c8a794a7",
+    "url": "/project-mercado-livre/index.html"
+  },
+  {
+    "revision": "513a6a66af5019c71b0e",
+    "url": "/project-mercado-livre/static/css/main.9e2e92db.chunk.css"
+  },
+  {
+    "revision": "c31b2fcd97a6e0f7eefc",
+    "url": "/project-mercado-livre/static/js/2.a573434a.chunk.js"
+  },
+  {
+    "revision": "513a6a66af5019c71b0e",
+    "url": "/project-mercado-livre/static/js/main.1bbfcc31.chunk.js"
+  },
+  {
+    "revision": "48e12c981e1e00ffc3be",
+    "url": "/project-mercado-livre/static/js/runtime~main.7c734680.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-mercado-livre/static/media/logo.5d5d9eef.svg"
+  }
+]);
